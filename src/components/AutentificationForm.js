2ca2eef21386124c962383786dd645137fa29363
Maschkov Konstantin <maschkovc@yandex.ru>
2023-11-15 21:23:49 +0300

    Валидация

--- src/components/AutentificationForm.js
+++ src/components/AutentificationForm.js
@@ -1,54 +1,36 @@
 import './autentificationForm.scss';
 import yLab from './Ylab.png';
 
-import { useEffect, useState } from 'react';
-import GetFromBD from '../services/GetFromBD';
-
-
+import { useState } from 'react';
 
 const AutentificationForm = () => {
 
 	const [userLogin, setUserLogin] = useState();
 	const [userPasword, setUserPasword] = useState();
-	console.log(userLogin)
-	console.log(userPasword)
 
-	const [user, setUser] = useState();
-	// const [loading, setLoading] = useState(true);
-	console.log(user)
+	const regex = new RegExp(/^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i);
+	console.log(regex.test(userLogin));
 
-	const { getUser } = GetFromBD();
-
-	useEffect(() => {
-		updateUser();
-	}, []);
+	// Валидация
 
 
-	const onUser = (newUser) => {
-		setUser(newUser);
+	const postData = (e) => {
+		e.preventDefault();
+		e.target.reset();
+
+		fetch('https://jsonplaceholder.typicode.com/posts', {
+			method: 'POST',
+			body: JSON.stringify({
+				email: userLogin,
+				password: userPasword,
+			}),
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8',
+			},
+		})
+			.then((response) => response.json())
+			.then((json) => console.log(json));	
 	}
-
-	const updateUser = () => {
-		getUser()
-			.then(onUser);
-	}
-
-
-	// kevin@gmail.com
-	// m38rmF$
-
-	const verification = (arr) => {
-		if (arr === undefined) {
-			return
-		} else {
-			arr.map(item => {
-				if (item.email === userLogin) {
-					alert('Вы авторизованы');
-				}
-			});
-		}
-	}
-
 
 	return (
 		<div className="autentificationForm">
@@ -56,23 +38,19 @@ const AutentificationForm = () => {
 				<img src={yLab} alt="YLaB" />
 			</div>
 			<div className="autentificationForm__formBlock">
-				<form onSubmit={verification(user)} className="autentificationForm__form">
+				<form onSubmit={postData} className="autentificationForm__form" >
 					<input className="autentificationForm__input" type="text" required placeholder="Логин" onChange={e => setUserLogin(e.target.value)} />
 					<input className="autentificationForm__input" type="text" required placeholder="Пароль" onChange={e => setUserPasword(e.target.value)} />
 					<div className='autentificationForm__checkBoxBlock'>
-						<input className='larger' required id="Checkbox" type="checkbox" />
+						<input className='larger' required id="Checkbox" type="checkbox"  />
 						<label className='autentificationForm__input label' id="Checkbox">Я согласен с условиями ресурса</label>
 					</div>
 					<button className='button'>Войти</button>
 				</form>
-
 			</div>
-
-
 		</div>
 
 	)
-
 }
 
 
